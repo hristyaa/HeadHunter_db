11face0fdeacc055e1c1d998c59ed91c002cd917
Hristyaa <shhs200799@gmail.com>
2025-10-02 16:51:04 +0900

    mypy src/hh_api.py+

--- src/hh_api.py
+++ src/hh_api.py
@@ -8,7 +8,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-DB_NAME = os.getenvis("DB_NAME")
+DB_NAME = os.getenv("DB_NAME")
 DB_USER = os.getenv("DB_USER")
 DB_PASSWORD = os.getenv("DB_PASSWORD")
 DB_HOST = os.getenv("DB_HOST")
